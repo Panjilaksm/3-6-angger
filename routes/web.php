a4715ca602249ca3aaa3cb689b8135f2568b36b3
panjilaksm <anggerpanji65@gmail.com>
2022-06-13 19:26:37 +0700

    modul 7

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Models\Contact;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,16 +20,44 @@ use Illuminate\Support\Facades\Route;
 
 
 route::get('/',function(){
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 route::get('/gallery',function(){
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
 Route::get('/about', function (){
     return view('about',[
+        "title" => "About",
         "nama" => "Angger Panji Laksmana",
         "email" => "3103120024@student.smktelkom-pwt.sch.id",
         "gambar" => "AIIM0839.JPG"
     ]);
+});
+Route::get('/gallery', function () {
+    return view('gallery',[
+        "title" => "Gallery"
+    ]);
+});
+
+Route::get('/admin  ', function(){
+    return view('admin.home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/delete_contact/{id}', ContactController::class . '@destroy');
+Route::get('/edit_contact/{id}', ContactController::class . '@edit');
+Route::post('/update_contact', ContactController::class . '@update');
+
+Route::resource('/contact', ContactController::class);
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
